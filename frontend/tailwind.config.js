c56bea733e88541bc6b49707975111ab303026dc
Arou03 <aroniaina.andriam@gmail.com>
2025-06-25 04:02:36 +0200

    [FRONTEND] Better visuals

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-	content: ['./src/**/*.{html,ts}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				'nice-font': ['nice-font'],
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "nice-font": ["nice-font"],
+      },
+      colors: {
+        "hogwarts-gold": "#B6862C",
+        "hogwarts-dark": "#1b1a17",
+        "hogwarts-panel": "#502F22",
+      },
+    },
+  },
+  plugins: [],
+};
